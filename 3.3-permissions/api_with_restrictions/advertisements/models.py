50729644777e2abfb1fde441ff7c248145300b86
Кирилл Соснюк <kirill-sosnyuk@yandex.ru>
2023-04-09 20:55:40 +0300

    restrictions

--- 3.3-permissions/api_with_restrictions/advertisements/models.py
+++ 3.3-permissions/api_with_restrictions/advertisements/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class AdvertisementStatusChoices(models.TextChoices):
-    """Статусы объявления."""
+    """Статусы объявления"""
 
     OPEN = "OPEN", "Открыто"
     CLOSED = "CLOSED", "Закрыто"
